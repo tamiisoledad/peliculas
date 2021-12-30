ea994671ff1439ffe0e21c7b3112e4234949f040
tamiisoledad <tamiisoledad4@gmail.com>
2021-12-30 00:27:07 -0300

    conectando base de datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let {index} = require("../controllers/indexController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index)
 
 module.exports = router;
